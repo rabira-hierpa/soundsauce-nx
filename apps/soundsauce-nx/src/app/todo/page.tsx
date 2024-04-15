e71338812535cc50bbcf5b45f19513472c370682
rabra <rabira.hierpa@gmail.com>
2024-04-15 12:02:25 +0300

    Add Todo page component

---
+++ apps/soundsauce-nx/src/app/todo/page.tsx
@@ -0,0 +1,160 @@
+'use client';
+import React from 'react';
+import {
+  TextField,
+  Button,
+  Typography,
+  Checkbox,
+  List,
+  ListItem,
+  Container,
+  makeStyles,
+} from '@material-ui/core';
+import { useState } from 'react';
+const StyledPage = ({ children }: { children: React.ReactNode }) => (
+  <div>{children}</div>
+);
+
+const useStyles = makeStyles({
+  input: {
+    width: '70%',
+    marginBottom: 30,
+  },
+  addButton: {
+    height: 55,
+    marginBottom: 30,
+  },
+  container: {
+    textAlign: 'center',
+    marginTop: 100,
+  },
+  list: {
+    width: '80%',
+    margin: 'auto',
+    display: 'flex',
+    justifyContent: 'space-around',
+    border: '1px solid light-gray',
+  },
+  text: {
+    width: '70%',
+  },
+  listButtons: {
+    marginLeft: 10,
+  },
+});
+
+export default function Todo() {
+  const [inputVal, setInputVal] = useState('');
+  const [todos, setTodos] = useState<any>([]);
+  const [isEdited, setIsEdited] = useState(false);
+  const [editedId, setEditedId] = useState(null);
+  const classes = useStyles();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVal(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (!isEdited) {
+      setTodos([
+        ...todos,
+        { val: inputVal, isDone: false, id: new Date().getTime() },
+      ]);
+    } else {
+      setTodos([...todos, { val: inputVal, isDone: false, id: editedId }]);
+    }
+    setInputVal('');
+    setIsEdited(false);
+  };
+
+  const onDelete = (id: any) => {
+    const newTodos = todos.filter((todo: any) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const handleDone = (id: any) => {
+    const updated = todos.map((todo: any) => {
+      if (todo.id === id) {
+        todo.isDone = !todo.isDone;
+      }
+      return todo;
+    });
+    setTodos(updated);
+  };
+
+  const handleEdit = (id: any) => {
+    const newTodos = todos.filter((todo: any) => todo.id !== id);
+    const editVal = todos.find((todo: any) => todo.id === id);
+    setEditedId(editVal.id);
+    setInputVal(editVal.val);
+    setTodos(newTodos);
+    setIsEdited(true);
+  };
+
+  return (
+    <StyledPage>
+      <div className="wrapper">
+        <div className="container">
+          <div className="flex justify-center">
+            <h1 className="text-6xl py-10 font-semibold text-violet-500">
+              Redux Toolkit{' '}
+            </h1>
+          </div>
+          <Container component="main" className={classes.container}>
+            <TextField
+              variant="outlined"
+              onChange={onChange}
+              label="Title"
+              value={inputVal}
+              className={classes.input}
+            />
+            <Button
+              size="large"
+              variant={isEdited ? 'outlined' : 'contained'}
+              color="primary"
+              onClick={handleClick}
+              className={classes.addButton}
+              disabled={inputVal ? false : true}
+            >
+              {isEdited ? 'Edit Task' : 'Add Task'}
+            </Button>
+            <List>
+              {todos.map((todo: any) => {
+                return (
+                  <ListItem key={todo.id} className={classes.list}>
+                    <Checkbox
+                      onClick={() => handleDone(todo.id)}
+                      checked={todo.isDone}
+                    />
+                    <Typography
+                      className={classes.text}
+                      style={{ color: todo.isDone ? 'green' : '' }}
+                      key={todo.id}
+                    >
+                      {todo.val}
+                    </Typography>
+                    <Button
+                      onClick={() => handleEdit(todo.id)}
+                      variant="contained"
+                      className={classes.listButtons}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      onClick={() => onDelete(todo.id)}
+                      color="secondary"
+                      variant="contained"
+                      className={classes.listButtons}
+                    >
+                      delete
+                    </Button>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Container>
+        </div>
+      </div>
+    </StyledPage>
+  );
+}
