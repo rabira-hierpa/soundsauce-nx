5af097c84c8414a11b74810804295ead2a808a5d
rabra <rabira.hierpa@gmail.com>
2024-04-19 22:53:52 +0300

    Add AppLayout component for layout structure in soundsauce-nx application

---
+++ apps/soundsauce-nx/src/app/app-layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex flex-col items-center px-4 py-2 md:py-10 sm:px-0">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default AppLayout;
